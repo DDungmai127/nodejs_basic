848a5766d488c297c393915d99226898f55c2e51
dungmaii <100128721+dungmaii@users.noreply.github.com>
2022-08-05 01:54:34 +0700

    explain EJS vs Controller

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -12,14 +12,22 @@ let getHomepage = (req, res) =>{
               data.push({
                 id: row.id,
                 email : row.email,
-                firstname : row.firstname,
-                lastname : row.lastname
+                firstname : row.firstName,
+                lastname : row.lastName
+              
               })
           }) 
           //console.log(fields);  fields contains extra meta data about results, if available
-        return res.render('index.ejs', {dataUser : JSON.stringify(data)} )   ;
+        return res.render('index.ejs', {dataUser : (data)} )   ;
+        // cái đoạn này, là truyền data qua view đấy. Cấu trúc hàm render nó gồm có render(file view, object data<đây sẽ là data>);
+        // object data được viết dưới dạng key : value, key sẽ được sử dụng trong file view nữa
+        // ta có thể truyền thêm data chỉ cần nó ngăn cách bởi dấu phẩy.
+        // ví dụ: return res.render('index.ejs', {dataUser : (data) , test : "Hello my friend"} )   ;
+        
 
+        // DO đó nó là cái vai trò của view engine nó giúp mình có thể code html mà vẫn có biến của controll
         }
+
       );
 }
 
